db137585cbec08f372a2a6ab1f615e9079112de0
TSB <122724396+tsbproject@users.noreply.github.com>
2024-02-11 15:41:27 -0800

    [committed]

--- src/index.js
+++ src/index.js
@@ -1,16 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ReactDOM from 'react-dom/client';
 // import './index.css';
 // import App from './App';
 
 import StarRating from './StarRating';
 
+function Test() {
+  const [movieRating, setMovieRating] = useState(0);
+  
+  return (
+    <div>
+      <StarRating color='blue' maxRating={10} onSetRating={setMovieRating} />
+
+      <p>This movie was rated {movieRating} Star</p>
+    </div>
+    
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating />
+    <StarRating maxRating={5} messages={["Terrible", "Bad", "Okay", 
+    "Good", "Amazing"]} />
+     <StarRating size='24' color='red' className="test" defaultRating={3} />
+    {/* <StarRating />   */}
+
+    <Test />
   </React.StrictMode>
 );
 
