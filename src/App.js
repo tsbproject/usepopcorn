1f1996fb0f1888064550f3608be1cdbca9658099
TSB <122724396+tsbproject@users.noreply.github.com>
2024-03-25 15:54:36 +0100

    [committed]

--- src/App.js
+++ src/App.js
@@ -52,20 +52,23 @@ const tempWatchedData = [
 export default function App() {
   const [movies, setMovies] = useState([]);
   const [watched, setWatched] = useState([]);
+  const [isLoading, setIsLoading] =useState(false)
   const query = "interstellar";
+  
   // const [isOpen2, setIsOpen2] = useState(true);
 
   useEffect(function() {
     async function fetchMovies() {
       const res= await fetch(
-        `http://www.omdbapi.com/?apikey=${KEY}&
-        s=${query}`
+        `http://www.omdbapi.com/?apikey=${KEY}&s=${query}`
    );
    const data = await res.json();
    setMovies(data.Search);
-   console.log(data.Search);
+   setIsLoading(false);
+  
  }
  fetchMovies();
+
 
   }, []);
 
@@ -82,7 +85,8 @@ export default function App() {
      </NavBar>
     <Main>
       
-      <Box movies = {movies}><MovieList movies={movies} />
+      <Box>
+       {isLoading ? <Loader /> : <MovieList movies={movies} />}
       
       </Box>
      
@@ -98,6 +102,12 @@ export default function App() {
     
      </>
   );
+}
+
+function Loader() {
+  return <p className="loader">Loading...</p>
+    
+  
 }
 
 
